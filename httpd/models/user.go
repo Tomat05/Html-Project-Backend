d5cb4a03b1d97e8a363f886d9155a22b09edc739
Tomat05 <64409983+Tomat05@users.noreply.github.com>
2022-11-03 20:22:45 +0000

    EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- httpd/models/user.go
+++ httpd/models/user.go
@@ -3,5 +3,5 @@ package models
 type User struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password string `json:"password"` // Yes I was using a string as the password to test the thing sue me
 }
